0c1d038df7fef20c19d30bd0402e60c7fc8f2853
mrv <mrv@vekiar.com>
2024-06-30 20:12:06 +0100

    (Re-) learning C. Working through "The C Programming Language" (K&R).
    Signed-off-by: mrv <mrv@vekiar.com>

---
+++ c_programming_language/003-f2c.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    float f, c;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    f = lower;
+    while (f <= upper) {
+        c = (5.0/9.0) * (f - 32.0);
+        printf("%3.0f %6.1f\n", f, c);
+        f = f + step;
+    }
+}
---
+++ c_programming_language/006-c2f.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int c;
+    printf("C\tF\n");
+    for (c = 0; c <= 200; c = c + 10)
+        printf("%d\t%6.1f\n", c, (c * (9.0/5.0) + 32));
+    return 0;
+}
