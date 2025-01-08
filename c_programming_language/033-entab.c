077bbc013ec4dafb3d04b5235f17b438c8182a9a
Mariano Vecchioli <mrv@vekiar.com>
2025-01-08 16:56:21 +0000

    Adding tabs everywhere!

--- c_programming_language/033-entab.c
+++ c_programming_language/033-entab.c
@@ -14,7 +14,7 @@ int main()
     int len;
     char line[MAXLINE];
     while (len = getlines(line, MAXLINE) > 0) {
-        printf("%s", line);
+        printf("%s\n", line);
     }
     return 0;
 }
@@ -22,13 +22,38 @@ int main()
 int getlines(char line[], int maxlines)
 {
     int i, c;
+    int spaces = 0;
+    int tabstop = 8;
     for (i = 0; i < maxlines && (c = getchar()) != EOF && c != '\n'; ++i) {
-        line[i] = c;
+        if (c != ' ') {
+            line[i] = c;
+            spaces = 0;
+            printf("%d:\t%c -> ", i, c);
+            for (int j = 0; j <= i; ++j) {
+                printf("%c", line[j]);
+            }
+            printf("\n");
+        } else if (c = ' ') {
+            ++spaces;
+            printf("%d:\tSpace detected -> %d sequential spaces (%d left for tabstop)\n", i, spaces, tabstop - spaces);
+            if (spaces >= tabstop) {
+                int offset = i - (spaces - 1);
+                printf("%d:\tInserting tabstop in position: %d\n", i, offset);
+                i = offset;
+                while (spaces >= tabstop) {
+                    line[i] = '\t';
+                    ++i;
+                    spaces = spaces - tabstop;
+                }
+                --i; // WHY do we need to do this ugly arse thing here?
+            } else {
+                line[i] = c;
+            }
+        }
     }
     if (i > 0) {
-        line[i] = '\n';
-        ++i;
         line[i] = '\0';
     }
+    printf("Array is %d characters long (including null terminator)\n", i);
     return i;
 }
